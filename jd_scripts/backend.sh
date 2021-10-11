712a888a16f976c4d4975d6e87ed1cfda62db3ba
root <root@rabbit.c.gold-totem-328117.internal>
2021-10-11 23:31:09 +0000

    Changes

--- jd_scripts/backend.sh
+++
@@ -1,9 +0,0 @@
-x=$(systemctl show -p ActiveState --value apache2)
-if [[ $x != *"inactive"* ]]
-then
-  echo "Back End is already running"
-else
-  echo "Starting up Back End"
-  sudo service apache2 start
-fi
-exit
