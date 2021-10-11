e2ba80e315a1dd3e2d07b7caef55f166a7abe810
francisperro <fp77@backend.c.backend490.internal>
2021-10-11 22:26:09 +0000

    file changes

---
+++ database/scripts/front.sh
@@ -0,0 +1,9 @@
+x=$(systemctl show -p ActiveState --value apache2)
+if [[ $x != *"inactive"* ]]
+then
+  echo "Apache Web Server is already running"
+else
+  echo "Starting up Apache Web Server"
+  sudo service apache2 start
+fi
+exit
