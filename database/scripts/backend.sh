07feb77f5af2cc19e6d1cd210d3bd76be65b7288
francisperro <fp77@backend.c.backend490.internal>
2021-11-02 22:43:52 +0000

    reciever script and cleanup

--- database/scripts/backend.sh
+++ database/scripts/backend.sh
@@ -1,9 +1,9 @@
 x=$(systemctl show -p ActiveState --value apache2)
 if [[ $x != *"inactive"* ]]
 then
-  echo "Front end is already running"
+  echo "Back End is already running"
 else
-  echo "Starting up Front end"
+  echo "Starting up Back End"
   sudo service apache2 start
 fi
 exit
