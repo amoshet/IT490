0c143f551210f02433081f266123c5efbf388643
root <root@rabbit.c.gold-totem-328117.internal>
2021-11-04 20:43:58 +0000

    commit

--- server.php
+++ server.php
@@ -11,22 +11,25 @@ $channel->queue_declare('hello', false, false, false, false);
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
 $callback = function ($msg) {
-        echo ' [x] Received ', $msg->body, "\n";
-        $v = json_decode($msg->body);
-        if ($v->{'type'} = "login"){
+        
+	$v = json_decode($msg->body);
+
+	if ($v->{'type'} = "login")
+	{
         $email = $v->{'email'};
         $password = $v->{'password'};
-        echo $email;
-        echo $password;
+        print $email;
+        print $password;
 	}
 
-	if *$v->{'type'} = "register"){
+	if ($v->{'type'} = "register" && isset($v->{'username'}))
+	{
 		$email2 = $v->{'email'};
 		$username2 = $v->{'username'};
 		$password2 = $v->{'password'};
-		echo $email2;
-		echo $username2;
-		echo $password2;
+		print $email2;
+		print $username2;
+		print $password2;
 
 	}
         
