113442a5e6dfc372a65d1695716b50cca5927565
Mathew Palikodath <mp649njit.edu>
2022-01-05 00:53:18 +0000

    Updated

--- backend/BackendServer.py
+++ backend/BackendServer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import pika, sys, os
 from DatabaseClient import databaseClient
 from APIClient import apiClient
@@ -10,63 +11,79 @@ channel.queue_declare(queue='BEServerqueue', exclusive=True)
 channel.queue_bind(exchange='BackEndExch', queue='BEServerQueue')
 
 def decider(type, rabbitMsg):
-	return:
-		'login' : loginFunc(rabbitMsg.get('email'), rabbitMsg.get('password'))
-    		'allRecipes' : allRecipes()
-		'searchRecipes' : searchRecipes(rabbitMsg.get('search'))
+	return{
+		'test' : lambda data : test(),
+		'login' : lambda data : loginFunc(rabbitMsg.get('email'), rabbitMsg.get('password')),
+#    		'allRecipes' : lambda data : allRecipes(rabbitMsq.get('search')),
+#		'searchRecipes' : lambda data : searchRecipes(rabbitMsg.get('search'))
+
+	}.get(type)(rabbitMsg)
 
 def on_request(ch, method, props, body):
     rabbitMSG = body
 
     print(rabbitMSG)
     rabbitResponse = decider(rabbitMSG.get('type'), rabbitMSG)
-
+    frontendReturn = rabbitResponse
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
                      properties=pika.BasicProperties(correlation_id = \
                                                          props.correlation_id),
-                     body=str(rabbitResponse))
+                     body=str(frontendReturn))
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='BEServerqueue', on_message_callback=on_request)
 
-print("Waiting for BackEnd Requests")
+print('Waiting for BackEnd Requests')
 channel.start_consuming()
 
+def test():
+	variable = "goodbye"
+	return variable
 
 def loginFunc(email, password):
-	SQLquery = "select Password from UserAccounts where Username=%(email)%"
-	SQLparameters = {'email':email}
-	DBpasser = {'query':SQLquery , 'parameters':SQLparameters}
-	DBclient = databaseClient()
-	DBresult = DBclient.call(DBpasser)
-	if DBresult.get('message' = password:
-		print("login success!")
-		return True
-	else:
-		print("login failed!")
-		return False
+	try:
+		SQLquery = "select Password from UserAccounts where Username=%(email)%"
+		SQLparameters = {'email':email}
+		DBpasser = {'query':SQLquery , 'parameters':SQLparameters}
+		DBclient = databaseClient()
+		DBresult = DBclient.call(DBpasser)
+		if DBresult.get('message' == password):
+			print("login success!")
+			return True
+		else:
+			print("login failed!")
+			return False
+	except:
+		print("Error in loginFunc")
 def registerFunc(email, password):
-	SQLquery = "insert Email and Password from UserAccounts"
-	SQLparameters = {'email':email , 'password':password}
-	DBpasser = {'query':SQLquery , 'parameters':SQLparameters}
-	DB client = databaseClient()
-	DBresult = DBclient.call(DBpasser)
-	if DBresult.
-		
+	try:
+		SQLquery = "insert Email and Password from UserAccounts"
+		SQLparameters = {'email':email , 'password':password}
+		DBpasser = {'query':SQLquery , 'parameters':SQLparameters}
+		DBclient = databaseClient()
+		DBresult = DBclient.call(DBpasser)
+		if DBresult.get('message' == password):
+			print('Registration success!')
+			return True
+		else:
+			print('Registration failed!')
+			return  False
+	except:
+		print("Error in registerFun")
+#def allRecipes(search)
+#	APIclient = apiClient()
+#	apiResponse = APIclient.call({
+#	'type': 'allRecipes',
+#	'query' : search})
+#	print(apiResponse)
+#	return apiResponse
 
-def allRecipes()
-	APIclient = apiClient()
-	apiResponse = APIclient.call({
-	'type': 'allRecipes'})
-	print(apiResponse)
-	return apiResponse
-
-def searchRecipes(search)
-	APIclient = apiClient()
-	apiResponse = APIclient.call({
-	'type': 'searchRecipe',
-	'query' : search})
-	print(apiResponse)
-	return apiResponse
+#def searchRecipes(search)
+#	APIclient = apiClient()
+#	apiResponse = APIclient.call({
+#	'type': 'searchRecipe',
+#	'query' : search})
+#	print(apiResponse)
+#	return apiResponse
