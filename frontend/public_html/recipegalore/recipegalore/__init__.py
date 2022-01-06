851bb8f38235c75551a2f46a8790c7c110e857bd
amoshet <ahmed.moshet@gmail.com>
2022-01-06 09:29:31 +0000

    flask now has most webpages running

--- frontend/public_html/recipegalore/recipegalore/__init__.py
+++ frontend/public_html/recipegalore/recipegalore/__init__.py
@@ -1,8 +1,34 @@
 import flask
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
-def hello():
-    return "Hello world!"
+def default():
+    return flask.render_template("index.html")
+
+@app.route('/index.html')
+def index():
+    return flask.render_template("index.html")
+
+@app.route('/login.html')
+def login():
+    return flask.render_template("login.html")
+
+#TODO change to function that redirects if not logged in
+@app.route('/home.html')
+def home():
+    return flask.render_template("home.html")
+
+@app.route('/register.html')
+def register():
+    return flask.render_template("register.html")
+
+#TODO add function for recipe search
+
+
+
+
+
 if __name__ == "__main__":
     app.run()
+
