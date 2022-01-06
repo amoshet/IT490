7432a0beb3b3da77ab0a1473b138e06f95d5b06b
Mathew Palikodath <mp649njit.edu>
2022-01-06 23:04:16 +0000

    Updated

---
+++ api/ApiServer.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+import pika, sys, os
+import requests
+
+credentials = pika.PlainCredentials('test', 'test')
+connection = pika.BlockingConnection(pika.ConnectionParameters('34.72.76.159',5672,'IT490',credentials))
+channel.exchange_declare(exchange='APIExch', exchange_type='direct')
+channel.queue_declare(queue='APIServerQueue', exclusive=True)
+channel.queue_bind(exchange='APIExch', queue='APIServerQueue')
+
+def decider(type, rabbitMsg):
+	return{
+		'test' : lambda data : test(),
+		'searchRecipes' : lambda data : searchRecipes(rabbitMsg.get('search'))
+
+	}.get(type)(rabbitMsg)
+
+def on_request(ch, method, props, body):
+    rabbitMSG = body
+
+    print(rabbitMSG)
+    rabbitResponse = decider(rabbitMSG.get('type'), rabbitMSG)
+    frontendReturn = rabbitResponse
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(frontendReturn))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='APIServerQueue', on_message_callback=on_request)
+
+print("Waiting for BackEnd Requests")
+channel.start_consuming()
+
+def test():
+	variable = "goodbye"
+	return variable
+
+def getAuto():
+url = "https://tasty.p.rapidapi.com/recipes/auto-complete"
+
+querystring = {"prefix":"chicken soup"}
+
+headers = {
+    'x-rapidapi-host': "tasty.p.rapidapi.com",
+    'x-rapidapi-key': "7512385757msh4aacdf9961a20c2p1225f7jsn8e19dfc61412"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+
+
+def getList():
+url = "https://tasty.p.rapidapi.com/recipes/list"
+
+querystring = {"from":"0","size":"20"}
+
+headers = {
+    'x-rapidapi-host': "tasty.p.rapidapi.com",
+    'x-rapidapi-key': "7512385757msh4aacdf9961a20c2p1225f7jsn8e19dfc61412"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+
+def getDetail():
+url = "https://tasty.p.rapidapi.com/recipes/detail"
+
+querystring = {"id":"5586"}
+
+headers = {
+    'x-rapidapi-host': "tasty.p.rapidapi.com",
+    'x-rapidapi-key': "7512385757msh4aacdf9961a20c2p1225f7jsn8e19dfc61412"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+#// add more functions that hit out to requests and return info
---
+++ database/DatabaseServer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import pika, sys, os
+import mysql-connector
+
+credentials = pika.PlainCredentials('test', 'test')
+connection = pika.BlockingConnection(pika.ConnectionParameters('34.72.76.159',5672,'IT490',credentials))
+channel.exchange_declare(exchange='DatabaseExch', exchange_type='direct')
+channel.queue_declare(queue='DBServerQueue', exclusive=True)
+channel.queue_bind(exchange='DatabaseExch', queue='DBServerQueue')
+
+def on_request(ch, method, props, body):
+    rabbitMSG = body
+    //rabbitMSG is going to come as a dictionary like this: rabbitMSG = { 'query': 'some sql statement', 'params' : 'the>
+    print(rabbitMSG) 
+    rabbitResponse = "goodbye from db"  //HERE is where you would replace this string after initial test and put a call >
+    frontendReturn = rabbitResponse
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(frontendReturn))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='DBServerQueue', on_message_callback=on_request)
+
+print("Waiting for BackEnd Requests")
+channel.start_consuming()
+
+def executeSQL(query,parameters):
+        mydb = mysql.connector.connect(
+        host="localhost",
+        user="fp77",
+        password="Au7K1a6cxFdj6MJ4",
+        database="fp77"
+        )
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+  print(x)
+
