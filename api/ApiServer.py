6ab4778c018ca795d260d24fd83650eb4f4ac11d
francisperro <fp77@backend.c.backend490.internal>
2022-01-07 00:14:01 +0000

    funny syntax errors

--- api/ApiServer.py
+++ api/ApiServer.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.8
 import pika, sys, os
 import requests
 
 credentials = pika.PlainCredentials('test', 'test')
 connection = pika.BlockingConnection(pika.ConnectionParameters('34.72.76.159',5672,'IT490',credentials))
+channel = connection.channel()
 channel.exchange_declare(exchange='APIExch', exchange_type='direct')
 channel.queue_declare(queue='APIServerQueue', exclusive=True)
 channel.queue_bind(exchange='APIExch', queue='APIServerQueue')
@@ -31,7 +32,7 @@ def on_request(ch, method, props, body):
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='APIServerQueue', on_message_callback=on_request)
 
-print("Waiting for BackEnd Requests")
+print("Waiting for Api Requests")
 channel.start_consuming()
 
 def test():
@@ -39,45 +40,45 @@ def test():
 	return variable
 
 def getAuto():
-url = "https://tasty.p.rapidapi.com/recipes/auto-complete"
+	url = "https://tasty.p.rapidapi.com/recipes/auto-complete"
 
-querystring = {"prefix":"chicken soup"}
+	querystring = {"prefix":"chicken soup"}
 
-headers = {
-    'x-rapidapi-host': "tasty.p.rapidapi.com",
-    'x-rapidapi-key': "7512385757msh4aacdf9961a20c2p1225f7jsn8e19dfc61412"
-    }
+	headers = {
+    		'x-rapidapi-host': "tasty.p.rapidapi.com",
+    		'x-rapidapi-key': "7512385757msh4aacdf9961a20c2p1225f7jsn8e19dfc61412"
+    	}
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+	response = requests.request("GET", url, headers=headers, params=querystring)
 
-print(response.text)
+	print(response.text)
 
 
 def getList():
-url = "https://tasty.p.rapidapi.com/recipes/list"
+	url = "https://tasty.p.rapidapi.com/recipes/list"
 
-querystring = {"from":"0","size":"20"}
+	querystring = {"from":"0","size":"20"}
 
-headers = {
-    'x-rapidapi-host': "tasty.p.rapidapi.com",
-    'x-rapidapi-key': "7512385757msh4aacdf9961a20c2p1225f7jsn8e19dfc61412"
-    }
+	headers = {
+		'x-rapidapi-host': "tasty.p.rapidapi.com",
+		'x-rapidapi-key': "7512385757msh4aacdf9961a20c2p1225f7jsn8e19dfc61412"
+	}
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+	response = requests.request("GET", url, headers=headers, params=querystring)
 
-print(response.text)
+	print(response.text)
 
 def getDetail():
-url = "https://tasty.p.rapidapi.com/recipes/detail"
+	url = "https://tasty.p.rapidapi.com/recipes/detail"
 
-querystring = {"id":"5586"}
+	querystring = {"id":"5586"}
 
-headers = {
-    'x-rapidapi-host': "tasty.p.rapidapi.com",
-    'x-rapidapi-key': "7512385757msh4aacdf9961a20c2p1225f7jsn8e19dfc61412"
-    }
+	headers = {
+		'x-rapidapi-host': "tasty.p.rapidapi.com",
+		'x-rapidapi-key': "7512385757msh4aacdf9961a20c2p1225f7jsn8e19dfc61412"
+	}
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+	response = requests.request("GET", url, headers=headers, params=querystring)
 
-print(response.text)
+	print(response.text)
 #// add more functions that hit out to requests and return info
