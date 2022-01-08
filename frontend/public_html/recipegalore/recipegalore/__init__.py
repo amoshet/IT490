747ba9066bc2ff0bcd3be5970c9d1f8c0f428315
amoshet <ahmed.moshet@gmail.com>
2022-01-08 01:58:54 +0000

    troubleshooting register function

--- frontend/public_html/recipegalore/recipegalore/__init__.py
+++ frontend/public_html/recipegalore/recipegalore/__init__.py
@@ -1,7 +1,7 @@
 from frontendClient import backendClient
 import pika
 import flask
-from flask import Flask, url_for, redirect, render_template, request
+from flask import Flask, url_for, redirect, render_template, request, flash
 #import sys
 #sys.path.insert(0, '/home/ahmed_moshet/IT490/frontend/public_html/recipegalore/recipegalore/')
 #import frontendclient
@@ -36,15 +36,33 @@ def login():
 	else:
 		return flask.render_template('login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if request.method == 'POST':
+		regemail = request.form['email']
+		regpass = request.form['p1']
+		regpass2 = request.form['p2']
+		#if regpass2 != regpass:
+		#	return flask.render_template('home.html')
+		dbchk = backendClient()
+		reginfo = dbchk.call({'type':'register', 'email':regemail, 'password':regpass})
+                #right now just returns whether successful or not
+		regStatus = reginfo.get('result')
+                #return redirect('/home', code=302)
+                #
+                #return redirect('/home', code=302)
+		return flask.render_template('register.html', regStatus)
+	else:
+		return flask.render_template('register.html')
 #TODO change to function that redirects if not logged in, and asks them to login first
 @app.route('/home')
 def home():
     return flask.render_template("home.html")
 
 #TODO function that if register.submit is pressed and db comes back true, redirect to home
-@app.route('/register')
-def register():
-    return flask.render_template("register.html")
+#@app.route('/register')
+#def register():
+#    return flask.render_template("register.html")
 
 #TODO add function for recipe search
 
