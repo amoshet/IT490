b863190bdf6e1cd2d332d92a98899c872685cb7d
amoshet <ahmed.moshet@gmail.com>
2022-01-09 14:13:39 +0000

    login/reg functions complete and live

--- frontend/public_html/recipegalore/recipegalore/__init__.py
+++ frontend/public_html/recipegalore/recipegalore/__init__.py
@@ -13,7 +13,6 @@ def default():
 def index():
 	return flask.render_template("index.html")
 
-#TODO function that if register.submit is pressed and db comes back true, redirect to home
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	if request.method == 'POST':
@@ -26,12 +25,21 @@ def login():
 		#TODO have it redirect to home and spit out the email
 		# +  saying welcome to the website
 		loginStatus = loginfo.get('result')
-		return flask.render_template("login.html", loginStatus)
+		if loginStatus == 'Login Success!':
+			flash('Login successful, Welcome back!')
+			return redirect('/home', code=302)
+		elif loginStatus == 'Login failed!':
+			flash('Incorrect credentials, please try again')
+			return redirect('/login', code=302)
+		else:
+			flash('Unknown error, please try again')
+			return redirect('/login', code=302)
 	else:
 		return flask.render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+	error = None
 	if request.method == 'POST':
 		regemail = request.form['email']
 		regpass = request.form['p1']
@@ -40,8 +48,8 @@ def register():
 		#TODO redirect to register again, but tell user passwords 
 		# + do not match
 		if regpass2 != regpass:
-			flash('Passwords do not match')
-			#return redirect('/index', code=302)
+			flash('Passwords do not match, please try again')
+			return redirect('/register', code=302)
 		dbchk2 = backendClient()
 		#sends register info to backend/db
 		reginfo = dbchk2.call({'type':'register', 'email':regemail, 'password':regpass})
@@ -49,9 +57,18 @@ def register():
 		#TODO have it redirect to login and say register successful
                 #TODO please login
 		regStatus = reginfo.get('result')
-		return flask.render_template("register.html", regStatus)
+		if regStatus == 'Registration Complete':
+			flash('Registration successful, Please login for the first time')
+			return redirect('/login', code=302)
+		elif regStatus == 'Registration Failed':
+			flash('Email already in use, please login with your account, or create a new one')
+			return redirect('/register', code=302)
+		else:
+			flash('Unknown error, please try again')
+			return redirect('/register', code=302)
 	else:
 		return flask.render_template('register.html')
+
 #TODO change to function that redirects if not logged in, and asks them to login first
 @app.route('/home')
 def home():
