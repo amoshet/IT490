04f700c477c3929edd11ea5fb2872f3a73e456e4
francisperro <fp77@backend.c.backend490.internal>
2022-01-10 21:51:33 +0000

    why godddddddddddddddd

--- database/DatabaseServer.py
+++ database/DatabaseServer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3.8
 import pika, sys, os
 import mysql.connector
-import simplejson as json
 
 credentials = pika.PlainCredentials('test', 'test')
 connection = pika.BlockingConnection(pika.ConnectionParameters('34.72.76.159',5672,'IT490',credentials))
@@ -10,42 +9,36 @@ channel.exchange_declare(exchange='DatabaseExch', exchange_type='direct')
 channel.queue_declare(queue='DBServerQueue', exclusive=True)
 channel.queue_bind(exchange='DatabaseExch', queue='DBServerQueue')
 
-def executeSQL(query, parameters):
-        mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="it490",
-        database="fp77"
-        )
-
-        mycursor = mydb.cursor()
-        mycursor.execute(query, parameters)
-        myresult = mycursor.fetchall()
-        splitter = myresult.split()
-        print (splitter)
-        myresult = myresult.replace("('", "")
-        myresult = myresult.replace("',)", "")
-        #myresult = myresult.split()
-        print (myresult[0])
-        variablee = {'message': myresult}
-        return variablee
-
 def on_request(ch, method, props, body):
-        rabbitMSG = json.loads(body.decode('utf-8'))
-
-        print(rabbitMSG)
-        rabbitResponse = executeSQL(rabbitMSG.get('query'), rabbitMSG.get('parameters'))
-        forJSON  = rabbitResponse
-        frontendReturn = json.JSONEncoder().encode(forJSON)
-        ch.basic_publish(exchange='',
-                routing_key=props.reply_to,
-                properties=pika.BasicProperties(correlation_id = \
-                        props.correlation_id),
-                body=frontendReturn)
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+    rabbitMSG = body
+    #rabbitMSG is going to come as a dictionary like this: rabbitMSG = { 'query': 'some sql statement', 'params' : 'the>
+    print(rabbitMSG)
+    rabbitResponse = "goodbye from db"  #//HERE is where you would replace this string after initial test and put a call >
+    frontendReturn = rabbitResponse
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(frontendReturn))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='DBServerQueue', on_message_callback=on_request)
 
 print("Waiting for Database Requests")
 channel.start_consuming()
+
+def executeSQL(query,parameters):
+	mydb = mysql.connector.connect(
+	host="localhost",
+	user="fp77",
+	password="Au7K1a6cxFdj6MJ4",
+	database="fp77"
+	)
+
+	mycursor = mydb.cursor()
+
+	mycursor.execute("SHOW TABLES")
+
+	for x in mycursor:
+		print(x)
